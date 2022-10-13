3ff8081fa030670664f58758f73e3572acf003c4
hkikura <90681228+hkikura@users.noreply.github.com>
2022-10-13 11:39:04 +0900

    Update 001.py

--- 001.py
+++ 001.py
@@ -3,15 +3,3 @@ b = 21
 print(f"a = {a}")
 print(f"b = {b}")
 print(f"a + b = {a + b}")
-'''
-# 磯野家　トイレ掃除当番
-import random
-mem = ["なみへい","ふね","さざえ","ますお","かつお","わかめ","たらちゃん"]
-cou = [0,0,0,0,0,0,0]
-for i in range(1,32):
-    tan = random.randint(0,5)
-    print("8月",i,"日--",mem[tan])
-    cou[tan] = cou[tan] + 1
-print("メンバー",mem)
-print("回　　数",cou)
-'''
